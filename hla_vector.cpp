92944260c53e5305e4b8374a45e3872c6493ff0b
Henry Neeb <henry.neeb@gmail.com>
2017-11-22 11:04:01 -0800

    implemented hla_vector constructor

---
+++ hla_vector.cpp
@@ -0,0 +1,16 @@
+
+#include "hla_vector.h"
+#include <vector>
+
+template <typename T>
+
+/* ctor */
+hla_vector<T>::hla_vector(std::vector<T> vec_) {
+    
+    vec = vec_;
+    
+}
+
+int main() {
+    return 0;
+}
---
+++ hla_vector.h
@@ -0,0 +1,23 @@
+
+#ifndef HLA_VECTOR_H
+#define HLA_VECTOR_H
+
+#include <vector>
+
+template <typename T>
+class hla_vector {
+    
+    std::vector<T> vec;
+    
+public:
+    
+    /* ctor */
+    hla_vector(std::vector<T> vec_);
+    
+private:
+    
+private:
+    
+};
+
+#endif
