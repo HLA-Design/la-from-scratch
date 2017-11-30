52eae50b41d9d81f4f0f35009c00c7bd34702058
Henry Neeb <henry.neeb@gmail.com>
2017-11-29 19:13:55 -0500

    matrix class skeleton

---
+++ hla_matrix.hpp
@@ -0,0 +1,24 @@
+
+#ifndef HLA_MATRIX_HPP
+#define HLA_MATRIX_HPP
+
+// includes : keep these to a minimum. Use forward declarations whenever can
+#include <vector>
+#include "hla_vector.hpp"
+
+template <typename T>
+class hla_matrix {
+    
+private:
+    
+public:
+    
+private:
+    
+private:
+    
+};
+
+#include "hla_matrix.inl"
+
+#endif
---
+++ hla_matrix.inl
@@ -0,0 +1,9 @@
+
+// includes : keep these to a minimum
+#include <vector>
+#include <cmath>
+#include <limits>
+#include <iostream>
+
+// Fwd declarations
+template <typename T> class hla_matrix;
